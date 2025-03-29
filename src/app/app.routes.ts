7a44d4effa5ee942e58762c46488520eb21890e4
sachin <sachin14051@gmail.com>
2025-03-29 19:10:57 +0530

    minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AddressComponent } from './address/address.component';
 import { DetailsComponent } from './details/details.component';
+import { authGuard } from './service/authguard';
 
 export const routes: Routes = [
+    {path:'address', component:AddressComponent,canActivate: [authGuard] },
     {path:'', component:LoginComponent},
     {path:'login', component:LoginComponent},
-    {path:'address', component:AddressComponent},
-    {path:'details', component:DetailsComponent},
+    {path:'details', component:DetailsComponent,canActivate: [authGuard] },
     {path:'pageNotFound', component:PageNotFoundComponent},
     {path:"**", redirectTo:'pageNotFound' }
 ];
--- src/app/service/authguard.service.spec.ts
+++ src/app/service/authguard.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthguardService } from './authguard.service';
+import { AuthguardService } from './authguard';
 
 describe('AuthguardService', () => {
   let service: AuthguardService;
